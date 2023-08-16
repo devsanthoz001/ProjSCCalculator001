7c348c7a2f55ff1a2edcf4af7a5e9a8fcbf1b62c
devsanthoz <devsanthoz@DESKTOP-KMRF5J5.localdomain>
2023-08-16 20:21:03 +0530

    Added 5 numbers

--- MathManager.cs
+++ MathManager.cs
@@ -4,6 +4,7 @@ public class MathManager
     {
         MathManager Calculate = new MathManager();
         Calculate.Add3Nos();
+        Calculate.Add5Nos(10);
     }
     public void Add3Nos()
     {
@@ -12,5 +13,14 @@ public class MathManager
         int c = 35;
         int sumof3Nos = a + b + c;
         System.Console.WriteLine(sumof3Nos);
+    }
+    public void Add5Nos(int a)
+    {
+        int b = 10;
+        int c = 21;
+        int d = 10;
+        int e = 14;
+        int sumof5Nos= a+b+c+d+e;
+        System.Console.WriteLine(sumof5Nos);
     }
 }
